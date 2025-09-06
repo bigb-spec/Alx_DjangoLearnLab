e28556c23de7e6727776dfe4a98f5d6cd4e758d6
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-06 18:51:57 +0300

    User restriction access

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,3 +20,9 @@ urlpatterns = [
         name="logout",
     ),
 ]
+
+urlpatterns = [
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
+]
