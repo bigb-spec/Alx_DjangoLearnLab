fca29c71d48aaaa0401313d606325a843018c39c
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-06 13:18:42 +0300

    Refactor views.py to include necessary imports and define list_books function for rendering book list

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+# function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    # render into HTML template
+    return render(request, "relationship_app/list_books.html", {"books": books})
