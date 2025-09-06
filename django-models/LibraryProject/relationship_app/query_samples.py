309fc791a4c931c60b04e90e96f90dd160f441d8
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-06 13:01:27 +0300

    Fix books_by_author function to correctly query books by author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,7 +10,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        books = author.books.all()
+        books = Book.objects.filter(author=author)
         return books
     except Author.DoesNotExist:
         return []
