7688035c29fa0c8e0645be1aa051a5e8c3bd63ae
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-06 14:49:15 +0300

    role-based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import user_passes_test # for role-based access
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Book
@@ -15,3 +16,29 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+# helpers to check role
+def is_admin(user):
+    return hasattr(user, "profile") and user.profile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "profile") and user.profile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "profile") and user.profile.role == "Member"
+
+
+# views for each role
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
