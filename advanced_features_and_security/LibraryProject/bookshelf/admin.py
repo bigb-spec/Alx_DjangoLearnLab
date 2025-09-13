4db658255195e7a104d07cc6fa81558de26d1963
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-13 15:56:18 +0300

    starting a fresh

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns shown in list view
-    list_filter = ('author', 'publication_year')            # Sidebar filters
-    search_fields = ('title', 'author')                     # Search bar fields
-
-admin.site.register(Book, BookAdmin)
