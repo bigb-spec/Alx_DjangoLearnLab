c796f818cbf8e8a790d399add2e45d90043c083b
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-23 17:04:51 +0300

    Refactor Book API views to enhance code readability and maintainability

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -19,8 +19,8 @@ from rest_framework import generics, permissions, filters
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend 
-
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework 
 
 # Book CRUD Views
 
