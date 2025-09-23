cba7aae20b3a27c166cf40315a2f4a1717abd76e
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-23 16:00:59 +0300

    Add initial models for Author and Book with migrations

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    Author model stores information about book authors.
+    One author can have many books.
+    """
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Book model stores information about individual books.
+    Each book is linked to a single Author (ForeignKey).
+    """
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
