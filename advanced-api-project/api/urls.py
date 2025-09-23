bdc747dd3e9271347b18e85bd325ed326f5cfbe6
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-23 16:28:21 +0300

    Add CRUD operation URLs for Book API

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import (
+    BookListView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+    BookDetailView,
+)
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
