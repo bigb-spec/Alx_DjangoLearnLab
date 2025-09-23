88d92e7fcd3bd6e8a8b52e25a87dbee834f9c91d
BigBrandon <archbrandonkiprotich@gmail.com>
2025-09-23 15:05:54 +0300

    Fix formatting of include statement in URL routing

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -6,6 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
      path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include("api.urls")),
 ]
 
