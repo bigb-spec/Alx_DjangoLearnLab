59ccaffc3d9b178b8509b8ddecdaa96f769dceca
BigBrandon <archbrandonkiprotich@gmail.com>
2025-10-04 15:04:39 +0300

    Defined a comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -17,6 +17,18 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_posted']
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
