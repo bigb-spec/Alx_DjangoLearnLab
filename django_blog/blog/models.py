f66e1bd91d5cfb81af718c6f896589939a5e9f96
BigBrandon <archbrandonkiprotich@gmail.com>
2025-10-04 14:57:49 +0300

    Added class profile in blo/models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -17,3 +17,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
