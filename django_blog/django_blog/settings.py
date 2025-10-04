af7498bfbaf9ece95d77e6afabd4989d1b6fe5c4
BigBrandon <archbrandonkiprotich@gmail.com>
2025-10-04 12:30:53 +0300

    Corrected spelling of statis in settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -126,14 +126,16 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "blog" / "static",
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
 
 
 TEMPLATES = [
